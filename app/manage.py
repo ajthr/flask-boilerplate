2eda6d407f5fa69bca456417413737f3adb09269
ajthr <ajithar204@gmail.com>
2021-07-30 00:42:57 +0530

    modified commands

--- app/manage.py
+++ app/manage.py
@@ -19,19 +19,33 @@ def execute_from_command_line(args):
         os.system("flask db upgrade")
     elif args[1] == "runserver":
         os.system("python app.py")
+    elif args[1] == "test":
+        os.system("python -m unittest discover")
+    elif args[1] == "--help":
+        print("\tFlask Boilerplate Help\n\n\
+        init        - Adds a migrations folder to the application.\n\
+        migrate     - Generates an initial migration.\n\
+        upgrade     - Applies the migration to the database.\n\
+        runserver   - Start the application server.\n\
+        test        - Run tests for the application.\n")
+    else:
+        raise RuntimeError(
+            "Couldn't find the command you just entered."
+            "use --help to view available commands."
+        )
 
 def main():
     # check if all required packages are installed and can be imported
-    try:
-        import flask
-        import flask_migrate
-        import flask_sqlalchemy
-        import marshmallow
-    except ImportError as exception:
-        raise ImportError(
-            "Couldn't import required packages."
-            "please install packages from requirements file."
-        ) from exception
+    # try:
+    #     import flask
+    #     import flask_migrate
+    #     import flask_sqlalchemy
+    #     import marshmallow
+    # except ImportError as exception:
+    #     raise ImportError(
+    #         "Couldn't import required packages."
+    #         "please install packages from requirements file."
+    #     ) from exception
     execute_from_command_line(sys.argv)
 
 if __name__ == '__main__':
